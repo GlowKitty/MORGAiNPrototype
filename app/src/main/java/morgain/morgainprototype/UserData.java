058e91451248806e49dc26993fe3fe8c77e4a965
GlowKitty <iamtheglowkitty@gmail.com>
2016-12-09 14:04:56 -0500

    user data class/object with saving properties

--- app/src/main/java/morgain/morgainprototype/UserData.java
+++ app/src/main/java/morgain/morgainprototype/UserData.java
@@ -1,5 +1,30 @@
 package morgain.morgainprototype;
 
-class UserData {
+import android.content.Context;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class UserData {
+
+    public UserData() {
+
+    }
+
+    public void saveData(UserData ud, Context ctx) {
+        String FILENAME = "MORGAiN_user_data";
+        try {
+            FileOutputStream fos = ctx.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(ud);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
